eed3858149fd8e8981382c10808dc71a60db9968
philliphnguyen <phillip.nguyen1997@yahoo.com>
2022-12-04 16:53:40 -0800

    value bug fixed

--- src/manager.cpp
+++ src/manager.cpp
@@ -30,6 +30,9 @@ int main(int argc, char *argv[]) {
     MPI_Comm second_intercomm;
     MPI_Comm_spawn("./worker", MPI_ARGV_NULL, 2, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm, MPI_ERRCODES_IGNORE);
     MPI_Comm_spawn("./worker", MPI_ARGV_NULL, 2, MPI_INFO_NULL, 0, MPI_COMM_SELF, &second_intercomm, MPI_ERRCODES_IGNORE);
+    //char* commands[1] = {"./worker"};
+    //int max_procs[1] = {1};
+    //MPI_Comm_spawn_multiple(1, commands, MPI_ARGVS_NULL, 2, max_procs, )
     int iter = 100;
 
     std::vector<MPI_Comm> comms;
@@ -92,24 +95,71 @@ int main(int argc, char *argv[]) {
     }
 
     while (true) {
-        int value = 0;
-        MPI_Status status;
-        MPI_Status second_status;
-        MPI_Recv(&value, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, intercomm, &status);
-        MPI_Recv(&value, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, second_intercomm, &second_status);
+        int value[2];
 
-        switch (status.MPI_TAG) {
+        MPI_Request requests[comms.size()];
+        for (int i = 0; i < comms.size(); i++) {
+            MPI_Irecv(&value[i], 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, comms.at(i), &requests[i]);
+        }
+
+        //Send work that was sent back to the manager since their queue was full
+        while (works.size() > 0) {
+            int tag = 0; //Action to perform
+            MPI_Status status;
+            //MPI_Send(&value, 1, MPI_INT, 0, tag, comms.at(0));
+            //MPI_Send(&value, 1, MPI_INT, 1, tag, comms.at(0));
+
+            int value = works.front();
+            works.pop_front();
+            printf("Manager sent %d to %d\n", value, childIndex);
+            MPI_Send(&value, 1, MPI_INT, childIndex, tag, comms.at(commIndex));
+            //value++;
+            //count++;
+
+            //Round robin under the assumption each intercomm has 2 processes
+            if (comms.size() > 1) {
+                if (commIndex == comms.size() - 1) commIndex = 0;
+                else commIndex++;
+            }
+            if (childIndex == 1) childIndex = 0;
+            else childIndex++;
+        }
+        
+        MPI_Status statuses[comms.size()];
+        MPI_Waitall(comms.size(), requests, statuses);
+        int return_value;
+        for (int i = 0; i < comms.size(); i++) {
+            printf("status source %d and status tag %d\n", statuses[i].MPI_SOURCE, statuses[i].MPI_TAG);
+            switch (statuses[i].MPI_TAG) {
             case 0:
-                printf("Manager Received: %d from Children Rank %03d\n", value, status.MPI_SOURCE);
+                printf("Manager Received: %d from Children Rank %03d\n", value[i], statuses[i].MPI_SOURCE);
+                break;
+            case 1:
+                printf("Manager received %d since capacity was full\n", value[i]);
+                works.push_back(value[i]);
+                break;
+            default:
+                // Unexpected message type 
+                MPI_Abort(MPI_COMM_WORLD, 1);
+            }
+        }
+
+        /*
+        for (int i = 0; i < comms.size(); i++) {
+            printf("status source %d and status tag %d\n", statuses[i].MPI_SOURCE, statuses[i].MPI_TAG);
+            switch (statuses[i].MPI_TAG) {
+            case 0:
+                printf("Manager Received: %d from Children Rank %03d\n", value, statuses[i].MPI_SOURCE);
                 break;
             case 1:
                 printf("Manager received %d since capacity was full\n", value);
                 break;
             default:
-                /* Unexpected message type */
+                // Unexpected message type 
                 MPI_Abort(MPI_COMM_WORLD, 1);
+            }
         }
-        
+       */
     }
 
     MPI_Finalize();
